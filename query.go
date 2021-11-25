7b96ae6df2ca6ea865f322956c2688f16735cbad
Viktor Mörsell <viktor@upper.st>
2021-11-25 17:17:00 +0100

    Start with the shortest postings list

--- query.go
+++ query.go
@@ -17,20 +17,35 @@ func NewQuerier(idx Index) Querier {
 }
 
 func (q *querier) Intersect(tokens ...string) ([]int, error) {
-	var res []int
+	if len(tokens) == 0 {
+		return nil, fmt.Errorf("no tokens provided")
+	}
 
+	postingsLists := make([][]int, 0, len(tokens))
+	var lowestDocFreqIdx int
+
+	// Fetch all postings lists.
 	for _, t := range tokens {
 		postings, err := q.idx.Postings(t)
 		if err != nil {
 			return nil, fmt.Errorf("get postings list: %w", err)
 		}
+		postingsLists = append(postingsLists, postings)
 
-		// Start with the full first postings list.
-		if res == nil {
-			res = postings
+		// Keep track of the shortest postings list. We will start with
+		// that one when doing the intersection.
+		if len(postingsLists[lowestDocFreqIdx]) > len(postings) {
+			lowestDocFreqIdx = len(postingsLists) - 1
+		}
+	}
+
+	res := postingsLists[lowestDocFreqIdx]
+	for i, l := range postingsLists {
+		if i == lowestDocFreqIdx {
 			continue
 		}
-		res = intersect(res, postings)
+
+		res = intersect(res, l)
 	}
 	return res, nil
 }
